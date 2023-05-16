d82e9597e20d669a0cf92b5582f252d716337115
Daniel Kobiałka <daniel.kobialka.stud@pw.edu.pl>
2023-05-16 22:14:33 +0200

    Added integration test with lexer

---
+++ code/tests/parser/IntegrationWithLexerTest.cpp
@@ -0,0 +1,72 @@
+#include <gtest/gtest.h>
+
+#include "../../parser/MyLangParser.h"
+#include "../../parser/PrintVisitor.h"
+#include "LexerMock/LexerMock.h"
+#include "../../lexer/MyLangLexer.h"
+
+TEST(IntegrationWithLexerTest, IntegrationAssignTest) {
+    std::istringstream iss("a = 3;");
+    bool errorOccurred = false;
+    auto onError = [&](Position position, ErrorType error) {
+        errorOccurred = true;
+    };
+    try {
+        MyLangLexer lexer(iss, onError);
+        MyLangParser parser(std::make_unique<MyLangLexer>(lexer), onError);
+        Program program = parser.parse();
+        std::ostringstream oss;
+        PrintVisitor visitor(oss);
+        program.accept(&visitor);
+        std::string expected = "program {\n"
+                               "  assign {\n"
+                               "    name {\n"
+                               "      identifier {\n"
+                               "        a\n"
+                               "      }\n"
+                               "    }\n"
+                               "    expression {\n"
+                               "      integer constant: 3\n"
+                               "    }\n"
+                               "  }\n"
+                               "}\n";
+        std::string result = oss.str();
+        ASSERT_EQ(result, expected);
+    } catch (...) {
+        errorOccurred = true;
+    }
+    ASSERT_FALSE(errorOccurred);
+}
+
+TEST(IntegrationWithLexerTest, IntegrationFunctionCallTest) {
+    std::istringstream iss("standardOutput(\"text\");");
+    bool errorOccurred = false;
+    auto onError = [&](Position position, ErrorType error) {
+        errorOccurred = true;
+    };
+    try {
+        MyLangLexer lexer(iss, onError);
+        MyLangParser parser(std::make_unique<MyLangLexer>(lexer), onError);
+        Program program = parser.parse();
+        std::ostringstream oss;
+        PrintVisitor visitor(oss);
+        program.accept(&visitor);
+        std::string expected = "program {\n"
+                               "  function call {\n"
+                               "    name {\n"
+                               "      identifier {\n"
+                               "        standardOutput\n"
+                               "      }\n"
+                               "    }\n"
+                               "    args {\n"
+                               "      string constant: text\n"
+                               "    }\n"
+                               "  }\n"
+                               "}\n";
+        std::string result = oss.str();
+        ASSERT_EQ(result, expected);
+    } catch (...) {
+        errorOccurred = true;
+    }
+    ASSERT_FALSE(errorOccurred);
+}
